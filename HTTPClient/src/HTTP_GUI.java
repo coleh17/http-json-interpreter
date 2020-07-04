708c59b46beaca284bdf2d1adbe6da1477624d5d
Cole <33649427+colehanson17@users.noreply.github.com>
2020-07-04 12:39:10 -0700

    cleaned up code

--- HTTPClient/src/HTTP_GUI.java
+++ HTTPClient/src/HTTP_GUI.java
@@ -16,9 +16,6 @@ public class HTTP_GUI {
 
 	private JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -32,16 +29,10 @@ public class HTTP_GUI {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
 	public HTTP_GUI() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(SystemColor.windowBorder);
@@ -49,37 +40,19 @@ public class HTTP_GUI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 	
-		
 		String res = "loading...";
-		
 		try {
 			res = HTTPRequest.getHTML("http://tacoshack.online:4041/richest");
 			System.out.println(HTTPRequest.getHTML("http://tacoshack.online:4041/richest"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println(res);
-		String [] lines = res.split("\n");
-		System.out.println(Arrays.toString(lines));
-//		JLabel label1 = new JLabel(lines[0]);
-//		panel.add(label1);
-		//for (String line : lines) {
-//		for (int i = 0; i < lines.length; i++) {
-//			System.out.println(i + " " + lines[i]);
-//			JLabel label = new JLabel(lines[i]);
-//			panel.add(label);
-//			System.out.println(label.getText());
-//		}
-		
+
 		JTextArea textArea = new JTextArea(res);
 		textArea.setFont(new Font("Arial Black", Font.BOLD, 13));
 		textArea.setBackground(new Color(204, 204, 204));
 		textArea.setBounds(25, 11, 364, 484);
 		frame.getContentPane().add(textArea);
 		textArea.setEditable(false);
-		
-//		JLabel lblNewLabel = new JLabel(res);
-//		lblNewLabel.setBounds(102, 54, 136, 67);
-//		frame.getContentPane().add(lblNewLabel);
 	}
 }
